c20bb1d6410ad25cac678f5f58f934171579040b
Tr.Nghia <t.nghia2112279@gmail.com>
2026-09-11 08:57:49 +0700

    chore(monorepo): configure linting, update prisma schema, and shared contracts

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
-  }
+  },
+  ignorePatterns: ["dist", "node_modules", "storybook-static", ".turbo"]
 };
